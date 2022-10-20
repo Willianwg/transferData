35bd72b2f9a473a14a96eb8b2aac5a6805ab22c8
Willian <willian.wg70@gmail.com>
2022-10-20 19:29:47 -0300

    Add the base of the application

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const sockets = new Server(server);
+
+const addresses = {}
+
+app.use(express.static('public'));
+
+sockets.on('connection', (socket)=>{
+    const userId = socket.id; 
+    const userIP = socket.handshake.address;
+
+    addresses[userIP] ? addresses[userIP].push(userId) : addresses[userIP] = [userId];
+
+    console.log('User: ' + userId + ' Entrou com o ip: ' + userIP);
+})
+
+server.listen(3000);
