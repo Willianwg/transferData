81be534183421cbbb679347f6dfcb38ca609025e
Willian Guedes <willian.wg70@gmail.com>
2022-10-21 14:12:32 +0000

    add closed comunication based in rooms

--- src/server.js
+++ src/server.js
@@ -7,29 +7,57 @@ const app = express();
 const server = http.createServer(app);
 const sockets = new Server(server);
 
-const addresses = {}
+const rooms = {};
 let txt = {}
+
+
+function isUserIn(userId, room){
+    const user = rooms[room].users.find(item=>item === userId);
+
+    return !!user;
+}
+
+function joinUser(userId, room){
+    if(isUserIn(userId,room)) return;
+
+    rooms[room].users.push(userId);
+    getMessage(room);
+}
+
+function createRoom(userId, room){
+     rooms[room] = [userId];
+
+     rooms[room] = {
+        users:[userId],
+        text:''
+     }
+}
+
+function getMessage(room){
+    rooms[room].users.map(id=>{
+        sockets.to(id).emit('message', rooms[room].text);
+    })
+}
 
 app.use( express.static(path.join(__dirname,'public')) );
 
 sockets.on('connection', (socket)=>{
     const userId = socket.id; 
     const userIP = socket.handshake.address;
-    
-    if(txt[userIP]){
-        socket.emit('transferData', txt[userIP]);
-    }
-
-    addresses[userIP] ? addresses[userIP].push(userId) : addresses[userIP] = [userId];
 
     console.log('User: ' + userId + ' Entrou com o ip: ' + userIP);
 
-    socket.on('transferData', (data)=>{
-        txt[userIP] = data;
+    socket.on('transferData', (roomData)=>{
+        const { room, text } = roomData;
+        rooms[room].text = text;
 
-        addresses[userIP].map(id=>{
-            sockets.to(id).emit('transferData', data);
-        })
+        getMessage(room);
+    })
+
+    socket.on('join', (room)=>{
+        console.log(room)
+        rooms[room] ? joinUser(userId, room) : createRoom(userId, room);
+        console.log(rooms[room])
     })
 
 })
