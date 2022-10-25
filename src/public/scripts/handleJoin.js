03c547b9edb6d721a1a7eaaddd4e9c81cf70284b
Willian <willian.wg70@gmail.com>
2022-10-25 14:10:41 -0300

    remove messages after after changing to another room

--- src/public/scripts/handleJoin.js
+++ src/public/scripts/handleJoin.js
@@ -1,5 +1,7 @@
-export default function handleJoin(event,socket,roomInput,user){
+export default function handleJoin(event,socket,roomInput,user, imageDiv, text){
     event.preventDefault();
     socket.emit('join', roomInput.value);
     user.changeRoom(roomInput.value);
+    imageDiv.innerHTML = '';
+    text.innerText = '';
 }
