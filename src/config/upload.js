35c8aab75021e7239c1311671dfaa7989b3d2f22
Willian Guedes <willian.wg70@gmail.com>
2022-10-29 14:14:26 +0000

    add limits of size and format to upload

--- src/config/upload.js
+++ src/config/upload.js
@@ -3,11 +3,26 @@ const path = require('path');
 
 module.exports = {
     storage:multer.diskStorage({
+
         destination:path.resolve(__dirname, '..','..','images'),
         filename:(req,file,callback)=>{
             const extension = path.extname(file.originalname);
             const name = path.basename(file.originalname,extension);
+
             callback(null, `${name}-${Date.now()}${extension}`);
         }
-    })
+    }),
+
+    fileFilter:(req,file,callback)=>{
+        const extension = path.extname(file.originalname);
+
+        if(extension !==  ".mp4" && extension !== ".png" && extension !== ".jpg" && extension !== ".pdf"){
+            return callback(null, false);
+        }
+        callback(null, true);
+    },
+
+    limits:{
+        fileSize:20 * 1000000
+    }
 }
