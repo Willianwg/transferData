dad55e3381b9c582a65fb448f50bcae4834fd4d7
elidianaandrade <lide_andrade_redes@hotmail.com>
2022-11-02 13:54:13 -0300

    Estilização da área de arquivos enviados e exibição do display de texto

--- src/public/scripts/handleJoin.js
+++ src/public/scripts/handleJoin.js
@@ -3,6 +3,7 @@ export default function handleJoin(event,socket,roomInput,user, imageDiv, text, 
     socket.emit('join', roomInput.value);
     user.changeRoom(roomInput.value);
     imageDiv.innerHTML = '';
-    text.innerText = '';
+    text.innerText = 'Nenhum texto enviado...';
+    text.style.display = 'flex';
     roomName.innerHTML = roomInput.value;
 }
